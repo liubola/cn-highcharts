ef6b12aa0e91d34cf106cfab1e503bbf9b8ab015
liubola <lby3523@gmail.com>
2020-11-26 19:52:53 +0800

    ss

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cn-highcharts',
-    version='0.0.5',
+    version='0.0.6',
     description=('Python Highcharts wrapper for china.'),
     install_requires=["Jinja2", "future"],
     # long_description=open('README.rst').read(),
